34d548bbdefaeb89542695d3febb58893c981bb0
Tania Jimenez <tnoir.jmz@gmail.com>
2024-01-08 22:51:26 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # proyecto_5
+
+Este proyecto contiene un código en donde arroja un menú de crear un histograma o un diagrama de dispersión al usuario. 
+Dependiendo de su elección se abre un segundo cuadro donde se puede escoger filtrar la tabla por marca de vehículo, actualizando así
+el diagrama escogido con anterioridad. 
---
+++ app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+car_data = pd.read_csv(
+    r'C:\Users\tnoir\Documents\Data scientist\Projects\proyecto_5\vehicles_us.csv')
+
+st.header('INFORMACION DE VEHICULOS')
+
+build_histogram = st.checkbox('Construir un histograma')
+build_scatter = st.checkbox('Construir un diagrama de dispersión')
+
+if build_histogram:  # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+
+    # Obtener todas las marcas únicas
+    marcas_unicas = car_data['model'].unique()
+
+    # Añadir una opción para seleccionar todas las marcas
+    marcas_unicas = ['Todas las marcas'] + list(marcas_unicas)
+
+    # Casillas de verificación para seleccionar las marcas
+    marcas_seleccionadas = st.multiselect(
+        'Selecciona los modelos:', marcas_unicas)
+
+    # Filtrar el conjunto de datos según las marcas seleccionadas
+    if 'Todas las marcas' in marcas_seleccionadas:
+        car_data_filtrado = car_data.copy()
+    else:
+        car_data_filtrado = car_data[car_data['model'].isin(
+            marcas_seleccionadas)]
+
+    # Crear un histograma para odómetros basado en las marcas seleccionadas
+    fig = px.histogram(car_data_filtrado, x='odometer',
+                       title='Histograma por odómetro y marca',
+                       labels={'odometer': 'Odómetro', 'model': 'Marca'})
+    st.plotly_chart(fig, use_container_width=True)
+
+
+elif build_scatter:  # si la casilla de verificación está seleccionada
+    st.write('Construir un diagrama de dispersión para la columna odómetro')
+    # Obtener todas las marcas únicas
+    marcas_s_unicas = car_data['model'].unique()
+
+    # Añadir una opción para seleccionar todas las marcas
+    marcas_s_unicas = ['Todas las marcas'] + list(marcas_s_unicas)
+
+    # Casillas de verificación para seleccionar las marcas
+    marcas_seleccionadas_sc = st.multiselect(
+        'Selecciona los modelos:', marcas_s_unicas)
+
+    # Filtrar el conjunto de datos según las marcas seleccionadas
+    if 'Todas las marcas' in marcas_seleccionadas_sc:
+        car_data_filtrado_sc = car_data.copy()
+    else:
+        car_data_filtrado_sc = car_data[car_data['model'].isin(
+            marcas_seleccionadas_sc)]
+
+    # Crear un diagrama de dispersión para odómetros basado en las marcas seleccionadas y en el precio
+    fig = px.scatter(car_data_filtrado_sc, x='odometer', y='price', title='Dispersión por odómetro y marca',
+                     labels={'odometer': 'Odómetro', 'model': 'Marca', 'price': 'Precio'}, color_discrete_sequence=['aquamarine'])
+    st.plotly_chart(fig, use_container_width=True)
